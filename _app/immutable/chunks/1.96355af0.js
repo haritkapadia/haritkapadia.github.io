1c32aafab9f84e398756dbd72b26b0cd486acfe9
Harit Kapadia <haritkapadia@outlook.com>
2023-10-01 21:14:36 -0400

    Added ts-movement-mode

---
+++ _app/immutable/chunks/1.96355af0.js
@@ -0,0 +1,1 @@
+import{default as t}from"../entry/error.svelte.60f06b9b.js";export{t as component};
---
+++ _app/immutable/chunks/2.7da3d332.js
@@ -0,0 +1,1 @@
+import{default as t}from"../entry/_page.svelte.34e647f8.js";export{t as component};

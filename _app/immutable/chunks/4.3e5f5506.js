e4a653849c0b93066bbb50adbbe1de628c742844
Harit Kapadia <haritkapadia@outlook.com>
2023-10-16 14:39:23 -0400

    Fixed content overfow

--- _app/immutable/chunks/4.3e5f5506.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../entry/resume-page.svelte.fad7d8c1.js";export{t as component};
---
+++ _app/immutable/chunks/4.6d21c969.js
@@ -0,0 +1,1 @@
+import{default as t}from"../entry/resume-page.svelte.3f2169a9.js";export{t as component};

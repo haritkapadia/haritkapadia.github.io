951ebe8383c556b19478cf96e621ac3c674a2a37
Harit Kapadia <haritkapadia@outlook.com>
2024-01-16 23:40:21 -0500

    Updated grades. Improved clarity of some resume points. Changed resume name to fixed size, improving how different fonts render letterhead.

--- _app/immutable/chunks/1.88963ac3.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../entry/error.svelte.2497202a.js";export{t as component};
---
+++ _app/immutable/chunks/4.20125454.js
@@ -0,0 +1,1 @@
+import{default as t}from"../entry/resume-page.svelte.ee3b9c72.js";export{t as component};
